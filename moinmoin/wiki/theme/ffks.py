fc09933f3e21a5bd2c8a40e84f2028e25d9b6412
Felix Möller <git@felix-moeller.net>
2015-06-22 18:43:45 +0200

    Added theme (WIP)

---
+++ moinmoin/wiki/theme/ffks.py
@@ -0,0 +1,183 @@
+# -*- coding: iso-8859-1 -*-
+"""
+    MoinMoin - Freifunk Kassel theme
+
+    @copyright: 2003-2005 Nir Soffer, Thomas Waldmann
+    @license: GNU GPL, see COPYING for details.
+"""
+
+from MoinMoin.theme import ThemeBase
+
+class Theme(ThemeBase):
+
+    name = "ffks"
+
+    def header(self, d, **kw):
+        """ Assemble wiki header
+
+        @param d: parameter dictionary
+        @rtype: unicode
+        @return: page header html
+        """
+        html = [
+            # Pre header custom html
+            self.emit_custom_html(self.cfg.page_header1),
+
+    		# Wrapper
+            u'<div class="wrapper">',
+
+            # Header
+            u'<div class="header-wrap">',
+            self.logo(),
+
+
+            u'<header class="header-wrap">',
+
+    		# v Social
+            u'<div class="social">',
+            u'<ul class="social-media-list">',
+
+    		# Mail
+            u'<li>',
+    		u'<a href="mailto:hallo@freifunk-kassel.de">',
+    		u'<i class="fa fa-envelope"></i>',
+    		u'hallo@freifunk-kassel.de',
+    		u'</a>',
+            u'</li>',
+
+    		# GitHub
+            u'<li>',
+    		u'<a target="_blank" href="https://github.com/freifunkks">',
+    		u'<i class="fa fa-github"></i>',
+    		u'freifunkks',
+    		u'</a>',
+            u'</li>',
+
+    		# Twitter 
+            u'<li>',
+    		u'<a target="_blank" href="https://twitter.com/freifunkks">',
+    		u'<i class="fa fa-twitter"></i>',
+    		u'freifunkks',
+    		u'</a>',
+            u'</li>',
+
+    		# RSS
+            u'<li>',
+    		u'<a target="_blank" href="https://twitter.com/freifunkks">',
+    		u'<i class="fa fa-rss"></i>',
+    		u'Neuigkeiten',
+    		u'</a>',
+            u'</li>',
+
+            u'</ul>',
+            u'</div>',
+    		# ^ Social
+
+
+            self.navibar(d),
+            #u'<hr id="pageline">',
+            #u'<div id="pageline"><hr style="display:none;"></div>',
+            self.msg(d),
+            u'</header>',
+
+            u'</div>',
+
+    		# do we need a search?
+            #self.searchform(d),
+
+    		# not used?!
+            #self.interwiki(d),
+            #self.trail(d),
+
+
+            # Post header custom html (not recommended)
+            self.emit_custom_html(self.cfg.page_header2),
+
+            self.editbar(d),
+            self.title(d),
+
+            # Start of page
+            self.startPage(),
+        ]
+        return u'\n'.join(html)
+
+    def editorheader(self, d, **kw):
+        """ Assemble wiki header for editor
+
+        @param d: parameter dictionary
+        @rtype: unicode
+        @return: page header html
+        """
+        html = [
+            # Pre header custom html
+            self.emit_custom_html(self.cfg.page_header1),
+
+            # Header
+            u'<div id="header">',
+            self.title(d),
+            self.msg(d),
+            u'</div>',
+
+            # Post header custom html (not recommended)
+            self.emit_custom_html(self.cfg.page_header2),
+
+            # Start of page
+            self.startPage(),
+        ]
+        return u'\n'.join(html)
+
+    def footer(self, d, **keywords):
+        """ Assemble wiki footer
+
+        @param d: parameter dictionary
+        @keyword ...:...
+        @rtype: unicode
+        @return: page footer html
+        """
+        page = d['page']
+        html = [
+            # End of page
+            self.pageinfo(page),
+            self.endPage(),
+
+            # Pre footer custom html (not recommended!)
+            self.emit_custom_html(self.cfg.page_footer1),
+
+            # Footer
+            u'<div id="footer">',
+
+    		# Two times needed?
+            #self.editbar(d),
+
+    		# Sorry, but you'll be mentioned somewhere else
+            #self.credits(d),
+            #self.showversion(d, **keywords),
+
+            self.username(d),
+            u'<ul id="footerpages">',
+            u'<li><a rel="nofollow" href="/wiki/&Auml;nderungen">&Auml;nderungen</a></li>',
+            u'<li><a rel="nofollow" href="/wiki/Hilfe">Hilfe</a></li>',
+            u'<li><a rel="nofollow" href="/wiki/Impressum">Impressum</a></li>',
+            u'</ul>',
+            u'</div>',
+
+
+            # Post footer custom html
+            self.emit_custom_html(self.cfg.page_footer2),
+
+    		# Wrapper
+            u'</div>',
+            ]
+        return u'\n'.join(html)
+
+
+def execute(request):
+    """
+    Generate and return a theme object
+
+    @param request: the request object
+    @rtype: MoinTheme
+    @return: Theme object
+    """
+    return Theme(request)
+
---
+++ moinmoin/wikiconfig.py
@@ -0,0 +1,50 @@
+# -*- coding: iso-8859-1 -*-
+# MoinMoin - Configuration for the Freifunk Kassel Wiki
+
+import sys
+import os
+
+from MoinMoin.config import multiconfig, url_prefix_static
+
+class Config(multiconfig.DefaultConfig):
+
+    # General MoinMoin Setup --------------------------------------------
+    # This has to be filled with your own config settings
+
+    # User interface ----------------------------------------------------
+    navi_bar = [
+        u'Info',
+        u'News',
+        u'Firmware',
+        u'Karte',
+        u'Kontakt',
+    ]
+
+    # Favicons everywhere
+    html_head = '''
+    <link rel="apple-touch-icon" sizes="57x57" href="/wikistatic/icons/apple-touch-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="/wikistatic/icons/apple-touch-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="/wikistatic/icons/apple-touch-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="/wikistatic/icons/apple-touch-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="/wikistatic/icons/apple-touch-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="/wikistatic/icons/apple-touch-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="/wikistatic/icons/apple-touch-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="/wikistatic/icons/apple-touch-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="/wikistatic/icons/apple-touch-icon-180x180.png">
+    <link rel="icon" type="image/png" href="/wikistatic/icons/favicon-32x32.png" sizes="32x32">
+    <link rel="icon" type="image/png" href="/wikistatic/icons/android-chrome-192x192.png" sizes="192x192">
+    <link rel="icon" type="image/png" href="/wikistatic/icons/favicon-96x96.png" sizes="96x96">
+    <link rel="icon" type="image/png" href="/wikistatic/icons/favicon-16x16.png" sizes="16x16">
+    <link rel="manifest" href="/wikistatic/icons/manifest.json">
+    <link rel="shortcut icon" href="/wikistatic/icons/favicon.ico">
+    <meta name="apple-mobile-web-app-title" content="Freifunk Kassel">
+    <meta name="application-name" content="Freifunk Kassel">
+    <meta name="msapplication-TileColor" content="#ffc40d">
+    <meta name="msapplication-TileImage" content="/wikistatic/icons/mstile-144x144.png">
+    <meta name="msapplication-config" content="/wikistatic/icons/browserconfig.xml">
+    <meta name="theme-color" content="#ffffff">'''
+
+    # when it looks acceptable
+    theme_default = u'ffks'
+    theme_force = True
+
